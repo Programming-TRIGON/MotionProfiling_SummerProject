21303165e9cfe938994cc1e3acebc083dc76d899
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-07-16 13:31:57 +0300

    finished drivetrain
    chatooleem ze seemcha chatooleem ze bracha

--- MotionProfiling_Project/src/main/java/frc/robot/DriveTrain.java
+++ MotionProfiling_Project/src/main/java/frc/robot/DriveTrain.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
@@ -12,11 +13,13 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  */
 public class DriveTrain extends Subsystem {
   
-  SpeedControllerGroup leftGroup, rightGroup; 
-  DifferentialDrive driveTrain;
-  ADXRS450_Gyro gyro;
-  Encoder leftEncoder, rightEncoder;
-  double TICKS_DIVIDER = 500;
+  private SpeedControllerGroup leftGroup, rightGroup; 
+  private DifferentialDrive driveTrain;
+  private ADXRS450_Gyro gyro;
+  private Encoder leftEncoder, rightEncoder;
+  private double TICKS_DIVIDER = 50;
+  private double prevLeftDistance, prevRightDistance, prevTime, leftVelocity, rightVelocity, leftAcceleration, rightAcceleration,
+   prevLeftVelocity, prevRightVelocity;
   
   public DriveTrain(SpeedController rearLeft, SpeedController frontLeft, SpeedController rearRight, SpeedController frontRight){
     this.rightGroup = new SpeedControllerGroup(rearRight, frontRight);
@@ -41,7 +44,6 @@ public class DriveTrain extends Subsystem {
     return this.leftEncoder.getDistancePerPulse() / TICKS_DIVIDER;
   }
 
-
   public double getRightEncoder(){
     return this.rightEncoder.getDistancePerPulse() / TICKS_DIVIDER;
   }
@@ -50,9 +52,48 @@ public class DriveTrain extends Subsystem {
     return (getLeftEncoder() + getRightEncoder()) / 2;
   }
 
+  public double getRighttVelocity(){
+    return this.rightVelocity;
+  }
+
+  public double getLeftVelocity(){
+    return this.leftVelocity;
+  }
+
+  public double getRightAcceleration() {
+    return this.rightAcceleration;
+  }
+
+  public double getLeftAcceleration() {
+    return this.leftAcceleration;
+  }
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+
+  @Override
+  public void periodic() {
+    double currentLeftDistance = getLeftEncoder();
+
+    double currentRightDistance = getRightEncoder();
+    double currentTime = Timer.getFPGATimestamp();
+
+    this.leftVelocity = (currentLeftDistance - prevLeftDistance) / (currentTime - prevTime);
+    this.leftAcceleration = (leftVelocity - prevLeftVelocity) / (currentTime - prevTime);
+
+    this.rightVelocity = (currentRightDistance - prevRightDistance) / (currentTime - prevTime);
+    this.rightAcceleration = (rightVelocity - prevRightVelocity) / (currentTime - prevTime);
+
+    this.prevLeftDistance = currentLeftDistance;
+    this.leftVelocity = prevLeftVelocity;
+
+    this.prevRightDistance = currentRightDistance;
+    this.rightVelocity = prevRightVelocity;
+
+    this.prevTime = currentTime;
+
   }
 }
