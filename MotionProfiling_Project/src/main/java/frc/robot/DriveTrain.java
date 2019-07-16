14611b6e11c95e168a1fd375455919c6ca9d9421
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-07-16 12:43:05 +0300

    added getangle + getencoder
    meow

--- MotionProfiling_Project/src/main/java/frc/robot/DriveTrain.java
+++ MotionProfiling_Project/src/main/java/frc/robot/DriveTrain.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -12,6 +14,8 @@ public class DriveTrain extends Subsystem {
   
   SpeedControllerGroup leftGroup, rightGroup; 
   DifferentialDrive driveTrain;
+  ADXRS450_Gyro gyro;
+  Encoder leftEncoder, rightEncoder;
   
   public DriveTrain(SpeedController rearLeft, SpeedController frontLeft, SpeedController rearRight, SpeedController frontRight){
     this.rightGroup = new SpeedControllerGroup(rearRight, frontRight);
@@ -26,6 +30,22 @@ public class DriveTrain extends Subsystem {
 
   public void arcadeDrive(double x, double y){
     this.driveTrain.arcadeDrive(y, x);
+  }
+
+  public double getAngle(){
+    return this.gyro.getAngle();
+  }
+
+  public double getLeftEncoder(){
+    return this.leftEncoder.getDistancePerPulse();
+  }
+
+  public double getRightEncoder(){
+    return this.rightEncoder.getDistancePerPulse();
+  }
+
+  public double getEncoders(){
+    return (getLeftEncoder() + getRightEncoder()) / 2;
   }
 
   @Override
