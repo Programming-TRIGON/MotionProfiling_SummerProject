1dfe27a66e2f5906fd848984cf868a47a0f7d1f6
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-07-18 13:53:55 +0300

    finished motion profiling no checking at all

--- MotionProfiling_Project/src/main/java/frc/robot/FollowPath.java
+++ MotionProfiling_Project/src/main/java/frc/robot/FollowPath.java
@@ -1,30 +1,46 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.followers.EncoderFollower;
 
-public class FollowPath extends Command { 
+public class FollowPath extends Command {
+
   int pathNumber;
   EncoderFollower right, left;
+  double leftCalculate, rightCalculate, gyroHeading, desiredHeading, angleDifference, turn;
 
   public FollowPath(int pathNumber) {
     requires(Robot.driveTrain);
     this.pathNumber = pathNumber;
+
     this.left = new EncoderFollower(Robot.pathCreater.getTrajectories(this.pathNumber)[0]);
     this.right = new EncoderFollower(Robot.pathCreater.getTrajectories(this.pathNumber)[1]);
   }
 
   @Override
   protected void initialize() {
-    this.left.configureEncoder((int)Robot.driveTrain.getLeftDistance(), 50, 0.1524);
+    this.left.configureEncoder(Robot.driveTrain.getLeftTicks(), 50, 0.1524);
     this.left.configurePIDVA(1.0, 0.0, 0.0, 1 / 80, 0);
   }
 
   @Override
   protected void execute() {
-    double l = left.calculate(Robot.driveTrain.getLeftDistance());
-    double r = right.calculate(Robot.driveTrain.getRightDistance());
+    this.leftCalculate = this.left.calculate(Robot.driveTrain.getLeftTicks());
+    this.rightCalculate = this.right.calculate(Robot.driveTrain.getRightTicks());
 
+    this.gyroHeading = Robot.driveTrain.getAngle();
+    this.desiredHeading = Pathfinder.r2d(this.left.getHeading());
+    this.angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
+
+    this.angleDifference = this.angleDifference % 360.0;
+    if (Math.abs(angleDifference) > 180.0) {
+  angleDiff = (angleDifference > 0) ? angleDifference - 360 : angleDiff + 360;
+} 
+
+    this.turn = 0.8 * (-1.0/80.0) * this.angleDifference;
+
+    Robot.driveTrain.tankDrive(this.leftCalculate + turn, this.rightCalculate - turn);
   }
 
   @Override
