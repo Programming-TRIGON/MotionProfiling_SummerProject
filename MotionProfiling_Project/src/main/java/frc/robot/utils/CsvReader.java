f5c5754ca87a886f5ea871d32fc4a3235ac1557d
EliyaFishman <45822259+eliyafishaman@users.noreply.github.com>
2019-08-05 10:21:46 +0300

    csv reader is now working

--- MotionProfiling_Project/src/main/java/frc/robot/utils/CsvReader.java
+++ MotionProfiling_Project/src/main/java/frc/robot/utils/CsvReader.java
@@ -1,39 +1,79 @@
 
 package frc.robot.utils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-
 import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Trajectory.Segment;
+
+import java.io.*;
 
 /**
  * Takes the csv file and translates it to a trajectory for the motionProfiling!
  */
 public class CsvReader {
 
-    public static Trajectory read(String path){
-        File file = new File(path);
+    public static Trajectory read(String path) {
+        Segment[] segments = null;
+        try {
+            segments = new Segment[countLines(path)];
+            File file = new File(path);
         FileReader r = new FileReader(file);
         BufferedReader br = new BufferedReader(r);
-        try {
-             String line;
-            while((line = br.readLine()) != null)
-                {
-                    String[] numbers = line.split(",");
-                }
-            }
-            catch(IOException e){
-                e.printStackTrace();
-            }
-            finally{
-                br.close();
-                r.close();
-            }
-        }
+            for (int i = 0; i < segments.length; i++) {
+                String line = br.readLine();
+                String[] numbers = line.split(",");
 
-    return yes
-}
+                double dt = Double.parseDouble(numbers[0]);
+                double x = Double.parseDouble(numbers[1]);
+                double y = Double.parseDouble(numbers[2]);
+                double p = Double.parseDouble(numbers[3]);
+                double v = Double.parseDouble(numbers[4]);
+                double a = Double.parseDouble(numbers[5]);
+                double h = Double.parseDouble(numbers[6]);
+                segments[i] = new Segment(dt, x, y, p, v, a, 0, h);
+            }
+            br.close();
+            r.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Trajectory(segments);
+    }
+
+    //taken from stackOverflow
+    private static int countLines(String filename) throws IOException {
+
+        try (InputStream is = new BufferedInputStream(new FileInputStream(filename))) {
+            byte[] c = new byte[1024];
+
+            int readChars = is.read(c);
+            if (readChars == -1) {
+                // bail out if nothing to read
+                return 0;
+            }
+
+            // make it easy for the optimizer to tune this loop
+            int count = 0;
+            while (readChars == 1024) {
+                for (int i = 0; i < 1024; ) {
+                    if (c[i++] == '\n') {
+                        ++count;
+                    }
+                }
+                readChars = is.read(c);
+            }
+
+            // count remaining characters
+            while (readChars != -1) {
+                System.out.println(readChars);
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+                readChars = is.read(c);
+            }
+
+            return count == 0 ? 1 : count;
+        }
+    }
 }
