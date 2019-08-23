5b055c296ffb8f5744690c8cdb6636d6c6efc133
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-08-23 10:38:24 +0300

    updated commands

--- MotionProfiling_Project/src/main/java/frc/robot/commands/TurnWithGyro.java
+++ MotionProfiling_Project/src/main/java/frc/robot/commands/TurnWithGyro.java
@@ -1,25 +1,20 @@
 package frc.robot.commands;
 
 import com.spikes2212.utils.PIDSettings;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.PIDController;
 import frc.robot.Robot;
 import frc.robot.utils.Limelight;
 import frc.robot.utils.Limelight.Target;
 
-import java.util.function.Supplier;
-
 
 public class TurnWithGyro extends Command {
-    private static final double TOLERANCE = 1;
-    private static final double DELTA_TOLERANCE = 1.0;
+    private double DELTA_TOLERANCE;
     private Target target;
     private PIDController pidController;
     private PIDSettings pidSettings;
 
-    public TurnWithGyro(Target target, PIDSettings pidSettings) {
+    public TurnWithGyro(Target target, PIDSettings pidSettings,double deltaTolerance) {
         requires(Robot.driveTrain);
         this.target = target;
         this.pidSettings = pidSettings;
@@ -29,9 +24,9 @@ public class TurnWithGyro extends Command {
     @Override
     protected void initialize() {
         Robot.limelight.setCamMode(Limelight.CamMode.vision);
-        Robot.limelight.setPipeline(target.getValue());
+        Robot.limelight.setPipeline(target.getIndex());
         pidController = new PIDController(pidSettings.getKP(), pidSettings.getKI(), pidSettings.getKD());
-        pidController.setAbsoluteTolerance(TOLERANCE, DELTA_TOLERANCE);
+        pidController.setAbsoluteTolerance(pidSettings.getTolerance(), DELTA_TOLERANCE);
         pidController.setInputRange(-27, 27);
         pidController.setOutputRange(-1, 1);
         pidController.setSetpoint(Robot.driveTrain.getAngle() + Robot.limelight.getTx());
--- MotionProfiling_Project/src/main/java/frc/robot/commands/VisionPID.java
+++ MotionProfiling_Project/src/main/java/frc/robot/commands/VisionPID.java
@@ -8,58 +8,60 @@ import frc.robot.utils.Limelight.CamMode;
 import frc.robot.utils.Limelight.Target;
 
 public class VisionPID extends Command {
-  double kp, ki, kd, tolrance, timer, DELTATIME;
-  int pipline;
-  PIDController pIDController;
-  public VisionPID(double kp, double ki, double kd, double tolrance, Target target) {
-    requires(Robot.driveTrain);
-    this.kp = kp;
-    this.ki = ki;
-    this.kd = kd;
-    this.tolrance = tolrance;
-    this.pipline = target.getValue();
-    DELTATIME = 25;
-  }
+    double kp, ki, kd, tolrance, lastTimeOnTarget, DELTA_TIME;
+    int pipline;
+    PIDController pIDController;
+  private double deltaTolerance;
 
-  @Override
-  protected void initialize() {
-    //setting pid values
-    pIDController = new PIDController(this.kp, this.ki, this.kd);
-    pIDController.setSetpoint(0);
-    pIDController.setInputRange(-27, 27);
-    pIDController.setOutputRange(-1,1);
-    pIDController.setAbsoluteTolerance(tolrance);
-    // setting limelight settings
-    Robot.limelight.setPipeline(pipline);
-    Robot.limelight.setCamMode(CamMode.vision);
-  }
-
-  @Override
-  protected void execute() {
-    //if it sees a target it will do pid on the x axis else it will not move
-    if(Robot.limelight.getTv()){
-    Robot.driveTrain.arcadeDrive(pIDController.calculate(Robot.limelight.getTx()), 0);
-    timer = Timer.getFPGATimestamp(); 
+  public VisionPID(double kp, double ki, double kd, double tolrance,double deltaTolarance, Target target) {
+        requires(Robot.driveTrain);
+        this.kp = kp;
+        this.ki = ki;
+        this.kd = kd;
+        this.tolrance = tolrance;
+        this.deltaTolerance = deltaTolarance;
+        this.pipline = target.getIndex();
+        DELTA_TIME = 25;
     }
-    else{
-      Robot.driveTrain.arcadeDrive(0, 0);
+
+    @Override
+    protected void initialize() {
+        //setting pid values
+        pIDController = new PIDController(this.kp, this.ki, this.kd);
+        pIDController.setSetpoint(0);
+        pIDController.setInputRange(-27, 27);
+        pIDController.setOutputRange(-1, 1);
+      pIDController.setAbsoluteTolerance(tolrance, deltaTolerance);
+        // setting limelight settings
+        Robot.limelight.setPipeline(pipline);
+        Robot.limelight.setCamMode(CamMode.vision);
     }
-  }
 
-  @Override
-  protected boolean isFinished() {
-    //if it does not detect a target for delta time it will return true
-    return pIDController.atSetpoint() || Timer.getFPGATimestamp() - timer > DELTATIME;
-  }
+    @Override
+    protected void execute() {
+        //if it sees a target it will do pid on the x axis else it will not move
+        if (Robot.limelight.getTv()) {
+            Robot.driveTrain.arcadeDrive(pIDController.calculate(Robot.limelight.getTx()), 0);
+            lastTimeOnTarget = Timer.getFPGATimestamp();
+        } else {
+            Robot.driveTrain.arcadeDrive(0, 0);
+        }
+    }
 
-  @Override
-  protected void end() {
-    Robot.driveTrain.arcadeDrive(0, 0);
-    Robot.limelight.setCamMode(CamMode.driver);
-  }
+    @Override
+    protected boolean isFinished() {
+        //if it does not detect a target for delta time it will return true
+        return pIDController.atSetpoint() || Timer.getFPGATimestamp() - lastTimeOnTarget > DELTA_TIME;
+    }
 
-  @Override
-  protected void interrupted() {
-    end();
-  }
+    @Override
+    protected void end() {
+        Robot.driveTrain.arcadeDrive(0, 0);
+        Robot.limelight.setCamMode(CamMode.driver);
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
+    }
 }
