2870541df2c400e2182c394457109c6ca73c6066
moshechelsky <MosheChelsky@gmail.com>
2019-08-23 12:09:49 +0300

    added pid setting and stuff to it

--- MotionProfiling_Project/src/main/java/frc/robot/commands/VisionWithVision.java
+++ MotionProfiling_Project/src/main/java/frc/robot/commands/VisionWithVision.java
@@ -1,60 +1,51 @@
 package frc.robot.commands;
 
-import com.spikes2212.utils.PIDSettings;
-
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.PIDController;
+import frc.robot.PidSettings;
 import frc.robot.Robot;
 import frc.robot.RobotConstants;
 import frc.robot.utils.Limelight.CamMode;
 import frc.robot.utils.Limelight.Target;
 
 public class VisionWithVision extends Command {
-  private double lastTimeOnTarget, deltaTime, ySetpoint, xSetpoint, deltaToleranceY, deltaToleranceX;
+  private double lastTimeOnTarget, deltaTime;
   private int pipline;
   private PIDController pIDControllerY, pIDControllerX;
-  private PIDSettings pidSettingsY, pidSettingsX;
+  private PidSettings pidSettingsY, pidSettingsX;
   private boolean isFollowingDistance;
 
-  public VisionWithVision(PIDSettings pidSettingsY, PIDSettings pidSettingsX, double ySetpoint, double xSetpoint,
-      double deltaTolaranceY, double deltaToleranceX, Target target, double deltaTime) {
+  public VisionWithVision(PidSettings pidSettingsY, PidSettings pidSettingsX, Target target, double deltaTime) {
     requires(Robot.driveTrain);
-    this.deltaToleranceY = deltaTolaranceY;
-    this.deltaToleranceX = deltaToleranceX;
     this.pipline = target.getIndex();
     this.deltaTime = deltaTime;
     this.pidSettingsY = pidSettingsY;
     this.isFollowingDistance = true;
-    this.ySetpoint = ySetpoint;
-    this.xSetpoint = xSetpoint;
   }
 
-  public VisionWithVision(PIDSettings pidSettingsX, double xSetpoint, double deltaTolaranceX, Target target,
-      double deltaTime) {
+  public VisionWithVision(PidSettings pidSettingsX, Target target, double deltaTime) {
     requires(Robot.driveTrain);
-    this.deltaToleranceX = deltaTolaranceX;
     this.pipline = target.getIndex();
     this.deltaTime = deltaTime;
     this.pidSettingsX = pidSettingsX;
     this.isFollowingDistance = false;
-    this.xSetpoint = xSetpoint;
   }
 
   @Override
   protected void initialize() {
     // setting pidY values
     pIDControllerX = new PIDController(pidSettingsX.getKP(), pidSettingsX.getKI(), pidSettingsX.getKD());
-    pIDControllerX.setSetpoint(this.xSetpoint);
+    pIDControllerX.setSetpoint(0);
     pIDControllerX.setInputRange(-27, 27);
     pIDControllerX.setOutputRange(-1, 1);
-    pIDControllerX.setAbsoluteTolerance(pidSettingsX.getTolerance(), deltaToleranceX);
+    pIDControllerX.setAbsoluteTolerance(pidSettingsX.getTolerance(), pidSettingsX.getDeltaTolerance());
     // setting pidX values
     if (isFollowingDistance) {
       pIDControllerY = new PIDController(pidSettingsY.getKP(), pidSettingsY.getKI(), pidSettingsY.getKD());
-      pIDControllerY.setSetpoint(RobotConstants.RobotDimensions.LIMELIGHT_DISTANCE_OFFSET + this.ySetpoint);
+      pIDControllerY.setSetpoint(RobotConstants.RobotDimensions.LIMELIGHT_DISTANCE_OFFSET + pidSettingsY.getSetpoint());
       pIDControllerY.setOutputRange(-1, 1);
-      pIDControllerY.setAbsoluteTolerance(pidSettingsY.getTolerance(), deltaToleranceY);
+      pIDControllerY.setAbsoluteTolerance(pidSettingsY.getTolerance(), pidSettingsY.getDeltaTolerance());
     }
     // setting limelight settings
     Robot.limelight.setPipeline(pipline);
